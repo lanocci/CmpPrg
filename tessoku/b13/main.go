ddddf437b23748bd21669baee6aec0a6bd00a8c2
lanocci <16540010+lanocci@users.noreply.github.com>
2025-05-22 17:46:56 +0900

    answered b13

---
+++ tessoku/b13/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, k int
+	fmt.Scan(&n)
+	fmt.Scan(&k)
+
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
+	}
+
+	r := make([]int, n)
+
+	for i := 0; i < n-1; i++ {
+		tmp := 0
+		for j := i; j < n; j++ {
+			if i == j {
+				tmp = a[j]
+			} else {
+				tmp += a[j]
+			}
+			if tmp > k {
+				break
+			}
+			r[i]++
+		}
+	}
+
+	answer := 0
+	for i := 0; i < n; i++ {
+		answer += r[i]
+	}
+	fmt.Println(answer)
+}
