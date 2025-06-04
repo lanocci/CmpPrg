5490222e6c99bd0812f0b52448725a18f01efd04
lanocci <16540010+lanocci@users.noreply.github.com>
2025-06-04 23:15:07 +0900

    answered a21

---
+++ tessoku/a21/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	p := make([]int, n+1)
+	a := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		fmt.Scan(&p)
+		fmt.Scan(&a)
+	}
+	dp := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, n+1)
+	}
+	dp[1][n] = 0
+	for len := n - 2; len >= 0; len-- {
+		for l := 1; l <= n-len; l++ {
+			r := l + len
+			s1 := 0
+			if l <= p[l-1] && p[l-1] <= r {
+				s1 = a[l-1]
+			}
+			s2 := 0
+			if p[r+1] <= r && p[r+1] >= l {
+				s2 = a[r+1]
+			}
+			if l == 1 {
+				dp[l][r] = dp[l][r+1] + s2
+			} else if r == n {
+				dp[l][r] = dp[l-1][r] + s1
+			} else {
+				dp[l][r] = int(math.Max(float64(s1), float64(s2)))
+			}
+
+		}
+	}
+	ans := 0
+	for i := 1; i <= n; i++ {
+		if dp[i][i] >= ans {
+			ans = dp[i][i]
+		}
+	}
+	fmt.Println(ans)
+}
